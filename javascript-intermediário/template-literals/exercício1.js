266834fc672beed62703d9fc94ad02b9fe5d0d3d
danildutra <danivetdutra@gmail.com>
2022-05-01 18:01:28 -0300

    Exercicio template-literals

---
+++ javascript-intermediário/template-literals/exercício1.js
@@ -0,0 +1,12 @@
+//Escreva uma frase que contenha seu nome, sua idade, e a sua atividade favorita.
+
+let nome = 'Daniella';
+let idade = 36;
+let atividade = {
+  dia1: 'trabalhar',
+  dia2: 'ir a praia',
+};
+
+console.log(
+  `Meu é ${nome}, minha idade ${idade} anos, minha ativadade favorita é ${atividade.dia2}`
+);
