8d243d9f84e4d654ff4559826f33f7234c727465
danildutra <danivetdutra@gmail.com>
2022-05-11 10:41:34 -0300

    Encadeamento de then s

--- node-aula-02/script2.js
+++ node-aula-02/script2.js
@@ -1,11 +1,27 @@
 //Criação da promessa
-const myPromise = new Promise((resolve, reject) => {
-  const nome = 'Elza';
+// const myPromise = new Promise((resolve, reject) => {
+//   const nome = 'Elza';
+//   nome == 'Daniella'
+//     ? resolve('Usuário Daniella encontrada')
+//     : reject('A usuário Daniella não foi encontrada');
+// });
+
+// myPromise.then((data) => {
+//   console.log(data);
+// });
+
+//Encadeamento de then
+const myPromise2 = new Promise((resolve, reject) => {
+  const nome = 'Daniella';
   nome == 'Daniella'
     ? resolve('Usuário Daniella encontrada')
     : reject('A usuário Daniella não foi encontrada');
 });
 
-myPromise.then((data) => {
-  console.log(data);
-});
+myPromise2
+  .then((data) => {
+    return data.toLowerCase();
+  })
+  .then((stringModificada) => {
+    console.log(stringModificada);
+  });
