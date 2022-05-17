df3820b64b1e9222c7c07fcbe62814b367685666
danildutra <danivetdutra@gmail.com>
2022-05-17 17:02:14 -0300

    Exemplo com bind e apply

--- programação-funcional-II/exemplo3.js
+++ programação-funcional-II/exemplo3.js
@@ -8,3 +8,19 @@ const somar2 = somar.bind({ base: 2 });
 const resultado = somar2(3);
 
 console.log(resultado); //5
+
+//No uso do bind precisa-se criar uma nova função:
+function func() {
+  console.log('x', this.x);
+}
+
+const funComX = func.bind({
+  x: 1,
+});
+funComX();
+
+//Com aplly não precisa de nova função, para executar basta passar o objeto para o apply:
+function fun2() {
+  console.log('y', this.y);
+}
+fun2.apply({ y: 3 });
