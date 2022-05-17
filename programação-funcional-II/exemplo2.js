64cd67d5b6bb67dc4a4678780279d188416d9caa
danildutra <danivetdutra@gmail.com>
2022-05-17 16:44:25 -0300

    Segundo exemplo de função passada como parâmetro para outra função usando map

--- programação-funcional-II/exemplo2.js
+++ programação-funcional-II/exemplo2.js
@@ -10,3 +10,9 @@ const somarCom2 = (x) => x + 2;
 const array1 = [1, 2, 3];
 const array2 = processar(array1, somarCom2);
 console.log(array2);
+
+// Usando map
+const somarComDois = (x) => x + 2;
+const array = [1, 2, 3];
+const arrayResult = array.map(somarComDois);
+console.log(arrayResult);
